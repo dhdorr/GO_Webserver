32c850dfad44e5a4578528d28342d0d3c6102f97
dhdorr <dhdorr@gmail.com>
2024-01-16 22:44:32 -0800

    update

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,8 +10,24 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	// Handler for "/app/" that serves the "index.html" from the root directory.
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+
+	// Handler for "/app/assets" that serves a different "index.html" from the "assets" directory.
+	mux.HandleFunc("/app/assets/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./assets/test.html")
+	})
+
+	mux.HandleFunc("/reset", apiCfg.resetHits)
+
+	mux.HandleFunc("/metrics", apiCfg.getMetrics)
+
 	mux.HandleFunc("/healthz", handlerReadiness)
 
 	corsMux := middlewareCors(mux)
@@ -41,4 +58,28 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// do something
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
+func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
 }
