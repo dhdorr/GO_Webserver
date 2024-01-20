17f880f68bf3855cbb5f0213c8d538bbaf355181
dhdorr <dhdorr@gmail.com>
2024-01-19 18:17:04 -0800

    crud ops

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -25,8 +27,11 @@ func main() {
 	rApi.Get("/healthz", handlerReadiness)
 	rApi.Get("/metrics", apiCfg.getMetrics)
 	rApi.Get("/reset", apiCfg.resetHits)
+	// rApi.Post("/validate_chirp", apiCfg.validateChirp)
+	rApi.Post("/chirps", apiCfg.validateChirp)
 
 	r.Mount("/api", rApi)
+	r.Mount("/admin", rApi)
 
 	corsMux := middlewareCors(r)
 
@@ -77,7 +82,87 @@ func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
+	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+	}
+
+	// cleaned := params.Body
+
+	// cleaned = strings.Replace(cleaned, "kerfuffle", "****", -1)
+	// cleaned = strings.Replace(cleaned, "sharbert", "****", -1)
+	// cleaned = strings.Replace(cleaned, "fornax", "****", -1)
+
+	cleaned2 := strings.Split(params.Body, " ")
+
+	for i, word := range cleaned2 {
+		temp := strings.ToLower(word)
+		if temp == "kerfuffle" || temp == "sharbert" || temp == "fornax" {
+			temp = "****"
+			cleaned2[i] = temp
+		} else {
+			cleaned2[i] = word
+		}
+		// temp = strings.Replace(strings.ToLower(word), "kerfuffle", "****", -1)
+		// temp = strings.Replace(strings.ToLower(temp), "sharbert", "****", -1)
+		// temp = strings.Replace(strings.ToLower(temp), "fornax", "****", -1)
+
+	}
+
+	cleaned3 := strings.Join(cleaned2, " ")
+
+	respBody := returnVals{
+		CleanedBody: cleaned3,
+	}
+
+	respondWithJSON(w, 200, respBody)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorVals struct {
+		Error string `json:"error"`
+	}
+	respBody := errorVals{
+		Error: msg,
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		w.WriteHeader(code)
+		return
+	}
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(dat)
+	return
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		respondWithError(w, 500, "Unable to marshal response")
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+	return
+
 }
