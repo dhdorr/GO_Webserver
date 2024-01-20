29f04bda8decc20c7a48f8d1c91bd0cbfb300ac8
dhdorr <dhdorr@gmail.com>
2024-01-19 20:08:28 -0800

    bad structure

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"sort"
 	"strings"
+	"sync"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -14,8 +18,16 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	db, err := NewDB("database.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("db created")
+
 	apiCfg := apiConfig{
-		fileserverHits: 0,
+		fileserverHits:   0,
+		successfulChirps: 0,
+		DB1:              db,
 	}
 
 	r := chi.NewRouter()
@@ -29,6 +41,7 @@ func main() {
 	rApi.Get("/reset", apiCfg.resetHits)
 	// rApi.Post("/validate_chirp", apiCfg.validateChirp)
 	rApi.Post("/chirps", apiCfg.validateChirp)
+	rApi.Get("/chirps", apiCfg.retrieveChirps)
 
 	r.Mount("/api", rApi)
 	r.Mount("/admin", rApi)
@@ -64,7 +77,9 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 }
 
 type apiConfig struct {
-	fileserverHits int
+	fileserverHits   int
+	successfulChirps int
+	DB1              *DB
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -92,7 +107,8 @@ func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
 		Body string `json:"body"`
 	}
 	type returnVals struct {
-		CleanedBody string `json:"cleaned_body"`
+		Body string `json:"body"`
+		Id   int    `json:"id"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -106,12 +122,6 @@ func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, 400, "Chirp is too long")
 	}
 
-	// cleaned := params.Body
-
-	// cleaned = strings.Replace(cleaned, "kerfuffle", "****", -1)
-	// cleaned = strings.Replace(cleaned, "sharbert", "****", -1)
-	// cleaned = strings.Replace(cleaned, "fornax", "****", -1)
-
 	cleaned2 := strings.Split(params.Body, " ")
 
 	for i, word := range cleaned2 {
@@ -122,19 +132,24 @@ func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
 		} else {
 			cleaned2[i] = word
 		}
-		// temp = strings.Replace(strings.ToLower(word), "kerfuffle", "****", -1)
-		// temp = strings.Replace(strings.ToLower(temp), "sharbert", "****", -1)
-		// temp = strings.Replace(strings.ToLower(temp), "fornax", "****", -1)
 
 	}
 
 	cleaned3 := strings.Join(cleaned2, " ")
 
+	cfg.successfulChirps += 1
+
+	uniqueId := cfg.successfulChirps
+
 	respBody := returnVals{
-		CleanedBody: cleaned3,
+
+		Body: cleaned3,
+		Id:   uniqueId,
 	}
 
-	respondWithJSON(w, 200, respBody)
+	cfg.DB1.CreateChirp(cleaned3)
+
+	respondWithJSON(w, 201, respBody)
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
@@ -165,4 +180,140 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Write(dat)
 	return
 
+}
+
+type Chirp struct {
+	Body string `json:"body"`
+	Id   int    `json:"id"`
+}
+
+type DBStructure struct {
+	Chirps map[int]Chirp `json:"chirps"`
+}
+
+func (cfg *apiConfig) retrieveChirps(w http.ResponseWriter, r *http.Request) {
+	dbChirps, err := cfg.DB1.GetChirps()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve Chirps")
+		return
+	}
+
+	chirps := []Chirp{}
+	for _, dbChirp := range dbChirps {
+		chirps = append(chirps, Chirp{
+			Id:   dbChirp.Id,
+			Body: dbChirp.Body,
+		})
+	}
+
+	sort.Slice(chirps, func(i, j int) bool {
+		return chirps[i].Id < chirps[j].Id
+	})
+
+	respondWithJSON(w, http.StatusOK, chirps)
+}
+
+type DB struct {
+	path string
+	mux  *sync.RWMutex
+}
+
+// NewDB creates a new database connection
+// and creates the database file if it doesn't exist
+func NewDB(path string) (*DB, error) {
+	db := &DB{
+		path: path,
+		mux:  &sync.RWMutex{},
+	}
+	err := db.ensureDB()
+
+	return db, err
+}
+
+// CreateChirp creates a new chirp and saves it to disk
+func (db *DB) CreateChirp(body string) (Chirp, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	id := len(dbStructure.Chirps) + 1
+	chirp := Chirp{
+		Id:   id,
+		Body: body,
+	}
+	dbStructure.Chirps[id] = chirp
+
+	err = db.writeDB(dbStructure)
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	return chirp, nil
+}
+
+// GetChirps returns all chirps in the database
+func (db *DB) GetChirps() ([]Chirp, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		fmt.Print(err)
+		return nil, err
+	}
+
+	chirps := make([]Chirp, 0, len(dbStructure.Chirps))
+	for _, chirp := range dbStructure.Chirps {
+		chirps = append(chirps, chirp)
+	}
+
+	return chirps, nil
+}
+
+// ensureDB creates a new database file if it doesn't exist
+func (db *DB) ensureDB() error {
+	_, err := os.ReadFile(db.path)
+	if errors.Is(err, os.ErrNotExist) {
+		//create db
+		dbStructure := DBStructure{
+			Chirps: map[int]Chirp{},
+		}
+		err2 := db.writeDB(dbStructure)
+		return err2
+	}
+
+	return err
+}
+
+// loadDB reads the database file into memory
+func (db *DB) loadDB() (DBStructure, error) {
+	db.mux.RLock()
+	defer db.mux.RUnlock()
+
+	dbStructure := DBStructure{}
+	dat, err := os.ReadFile(db.path)
+	if errors.Is(err, os.ErrNotExist) {
+		return dbStructure, err
+	}
+	err = json.Unmarshal(dat, &dbStructure)
+	if err != nil {
+		return dbStructure, err
+	}
+
+	return dbStructure, nil
+}
+
+// writeDB writes the database file to disk
+func (db *DB) writeDB(dbStructure DBStructure) error {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	dat, err := json.Marshal(dbStructure)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(db.path, dat, 0600)
+	if err != nil {
+		return err
+	}
+	return nil
 }
